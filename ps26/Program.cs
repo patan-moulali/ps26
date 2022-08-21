86a2286025b596502233f6ff051c6278e06dc303
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-21 15:07:11 +0530

    Add project files.

---
+++ ps26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, r, sum = 0, t;
+
+            Console.Write("\n\n");
+            Console.Write("Display the number in reverse order:\n");
+            Console.Write("--------------------------------------");
+            Console.Write("\n\n");
+
+
+            Console.Write("Input a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (t = num; t != 0; t = t / 10)
+            {
+                r = t % 10;
+                sum = sum * 10 + r;
+            }
+            Console.Write("The number in reverse order is : {0} \n", sum);
+            Console.Read();
+        }
+    }
+}
